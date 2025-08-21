d1afb91606913580889e8e1c90f57fc86d17afcc
Reyhansayah11 <mr.reihansyah@gmail.com>
2025-08-21 11:44:25 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // URL frontend Anda
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
